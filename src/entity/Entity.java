0d53d40908fca0273179f65a222ee7743180968a
Arkandhaf <arkadhaifulloh@gmail.com>
2025-12-28 22:18:41 +0700

    Nambahin GUI Player

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,13 +3,16 @@ package entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage; // <--- INI BARU (Buat nampung gambar)
 
 public abstract class Entity {
     public double x, y;
     public int width, height;
     public double speed;
     public Color color;
-    public boolean alive = true; // Status hidup/mati
+    public boolean alive = true;
+    
+    public BufferedImage image; // <--- INI VARIABEL BARU
 
     public Entity(double x, double y, int width, int height, double speed, Color color) {
         this.x = x;
@@ -23,11 +26,17 @@ public abstract class Entity {
     public abstract void update();
     
     public void draw(Graphics2D g2) {
-        g2.setColor(color);
-        g2.fillRect((int)x, (int)y, width, height);
+        // LOGIKA GAMBAR:
+        // Kalau ada gambar (image != null), kita gambar pakai image.
+        // Kalau gak ada (null), kita gambar kotak biasa (misal buat peluru).
+        if (image != null) {
+            g2.drawImage(image, (int)x, (int)y, width, height, null);
+        } else {
+            g2.setColor(color);
+            g2.fillRect((int)x, (int)y, width, height);
+        }
     }
 
-    // Untuk Cek Tabrakan (Hitbox)
     public Rectangle getBounds() {
         return new Rectangle((int)x, (int)y, width, height);
     }
--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,14 +1,29 @@
 package entity;
 
-import inputs.KeyHandler; // <--- Wajib Import karena beda package
+import inputs.KeyHandler;
 import java.awt.Color;
+import java.io.IOException;       // <--- Import buat baca file error
+import javax.imageio.ImageIO;     // <--- Import buat baca gambar
 
 public class Player extends Entity {
     KeyHandler keyH;
 
     public Player(int x, int y, KeyHandler keyH) {
-        super(x, y, 32, 32, 4, Color.BLUE); // Player = Kotak Biru
+        // Ubah ukuran jadi 48x48 (atau sesuaikan sama gambarmu) biar agak gedean
+        // Color.BLUE kita biarin aja buat cadangan kalau gambar gagal loading
+        super(x, y, 48, 48, 4, Color.BLUE); 
         this.keyH = keyH;
+
+        getPlayerImage(); // <--- Panggil fungsi load gambar
+    }
+
+    public void getPlayerImage() {
+        try {
+            // UBAH DARI .png JADI .jpg DI SINI
+            image = ImageIO.read(getClass().getResourceAsStream("/res/player.JPG"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
